7c9dcfa5dbea03591e0dbad47f5ae26bdfe8b50c
meeyoungchoi <goodlife1359@gmail.com>
2022-04-16 22:09:59 +0900

    Lombok을 사용하여 생성자 의존관계 주입을 @RequiredArgsConstructor 어노테이션을 대체

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("asdfg");
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
